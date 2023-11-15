2ec8a89cf123452038fbbe72ab9d895a86483c1d
João Gabriel De Jesus Cunha <141980608+jogabrieldev@users.noreply.github.com>
2023-11-15 12:27:46 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,8 @@
+let formulario = document.querySelector(".fale-conosco")
+console.log(formulario)
+function clicarBotao(){
+    formulario.style.left = "700px"
+}
+function sumirForulario(){
+    formulario.style.left = "-320px"
+}
